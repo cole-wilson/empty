a18881a5ca68e5bb96b97d81b26a391914ab049e
cole wilson <cole@colewilson.xyz>
2021-02-01 19:38:55 +0000

    a

---
+++ dist/homebrew/empty_project.rb
@@ -0,0 +1,25 @@
+# Created with sailboat, the Python releaser
+
+# v0.0.1+43c3eda.2
+
+class empty_project < Formula
+  include Language::Python::Virtualenv
+
+  desc "empty project for sailboat testing"
+  homepage "https://github.com/cole-wilson/empty"
+  url "{pyhosted}" # These lines must be configured during release, not build.
+  sha256 "{sha256}" # ^^^
+  license "none"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "python@3.9"
+
+
+
+  def install
+    virtualenv_install_with_resources
+  end
+end
